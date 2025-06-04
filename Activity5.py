252cbecce805786ac718ca5fedfc43a5c6636c9d
sewwandi789 <117725276+sewwandi789@users.noreply.github.com>
2025-06-04 15:38:35 +0530

    Add files via upload

---
+++ Activity5.py
@@ -0,0 +1,6 @@
+val1 = int(input("Enter first number: "))
+val2 = int(input("Enter second number: "))
+val3 = int(input("Enter third number: "))
+
+print("Maximum number is: ", max(val1, val2, val3))
+print("Minimum number is: ", min(val1, val2, val3))
